b1d031852c8b3696762f62bb23d9fa81caf6217c
aleapproved <149070115+aleapproved@users.noreply.github.com>
2025-08-19 23:25:45 +0100

    Add files via upload

---
+++ pages.js
@@ -0,0 +1,6 @@
+window.SITE_PAGES = [
+  { slug: "index.html", label: "home" },
+  { slug: "malaphors.html", label: "malaphors" },
+  { slug: "cv.html", label: "cv" },
+  { slug: "https://www.linkedin.com/in/alessandrogillies/", label: "linkedin" }
+];

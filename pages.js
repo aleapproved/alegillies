c9505becc6d081874df64f6e54416c443cf27053
aleapproved <149070115+aleapproved@users.noreply.github.com>
2025-08-25 20:15:08 +0100

    Update files

--- pages.js
+++ pages.js
@@ -2,5 +2,6 @@ window.SITE_PAGES = [
   { slug: "/", label: "home" },
   { slug: "/malaphors/", label: "malaphors" },
   { slug: "/cv/", label: "cv" },
+  { slug: "/contact/", label: "contact" },
   { slug: "https://www.linkedin.com/in/alessandrogillies/", label: "linkedin" }
 ];
